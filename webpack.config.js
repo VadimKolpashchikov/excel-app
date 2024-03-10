29f1cb157ced6f02c0da11422ee743ab9e191902
VadimKolpashchikov <70956901+VadimKolpashchikov@users.noreply.github.com>
2024-03-10 19:14:45 +0300

    feat: финишь настройки конфига проекта webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,86 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-module.exports = {
-  context: path.resolve(__dirname, "src"),
-  mode: "development",
-  entry: "./index.js",
-  output: {
-    filename: "bundle.[hash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  resolve: {
-    extensions: [".js"],
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@core": path.resolve(__dirname, "src/core"),
-    },
-  },
-  plugins: [
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const setBundleExtension = (ext) => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
+const getPlugin = () => {
+  const plugins = [
     new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
-      template: "index.html",
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      },
     }),
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src/favicon.ico"),
-          to: path.resolve(__dirname, "dist"),
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
         },
       ],
     }),
     new MiniCssExtractPlugin({
-      filename: "bundle.[hash].css",
+      filename: setBundleExtension('css'),
     }),
-  ],
+  ];
+
+  if (isDev) {
+    plugins.push(
+      new ESLintPlugin(),
+    );
+  }
+
+  return plugins;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: ['@babel/polyfill', './index.js'],
+  output: {
+    filename: setBundleExtension('js'),
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.scss'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core'),
+    },
+  },
+  devtool: isDev ? 'source-map' : false,
+  devServer: {
+    port: 3000,
+    hot: isDev,
+  },
+  plugins: getPlugin(),
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
