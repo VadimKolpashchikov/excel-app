1161abc264c3db0fb27e80c2b0dd0366c20d2224
VadimKolpashchikov <70956901+VadimKolpashchikov@users.noreply.github.com>
2024-08-06 22:14:02 +0300

    feat: класс эмиттера

---
+++ src/core/Emitter.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/prefer-default-export */
+export class Emitter {
+  constructor() {
+    this.listeners = {};
+  }
+
+  emit(event, ...args) {
+    this.listeners[event]?.forEach((listener) => {
+      listener(...args);
+    });
+  }
+
+  subscribe(event, fn) {
+    this.listeners[event] = this.listeners[event] ?? [];
+    this.listeners[event].push(fn);
+
+    return () => {
+      this.listeners[event] = this.listeners[event]
+        .filter((listener) => listener !== fn);
+    };
+  }
+}
